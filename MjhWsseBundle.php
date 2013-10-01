6bdced488e2cfd5d9ac553461c50a613782ccc53
Ciprian Vieru <ciprian.vieru@webflow.ro>
2013-10-01 17:30:20 +0300

    - register as security listener

--- MjhWsseBundle.php
+++ MjhWsseBundle.php
@@ -3,7 +3,16 @@
 namespace MJH\WsseBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use MJH\WsseBundle\DependencyInjection\Security\Factory\WsseFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class MjhWsseBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new WsseFactory());
+    }
 }
